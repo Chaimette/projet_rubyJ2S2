441f9f2fa35591ad5ead2970f0a173d2fb8449da
Chaima <chaima_elkadiri@hotmail.fr>
2023-07-04 23:46:26 +0200

    All done

--- exo_10.rb
+++ exo_10.rb
@@ -1,12 +1,12 @@
-# Demande l'année de naissance à l'utilisateur
+
 puts "What year were you born? "
 print ">"
 annee_naissance = gets.chomp.to_i
 
-# Obtient l'année actuelle
+
 annee_actuelle = Time.now.year
 
-# Boucle pour afficher chaque année depuis l'année de naissance jusqu'à l'année actuelle
+
 (annee_naissance..annee_actuelle).each do |annee|
   age = annee_actuelle - annee
   puts "#{annee} --> #{age} an(s)"
